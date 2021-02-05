e30f06418a32a7554de7a9cd11ed70a3b15384bd
Seth Paydar <sethpaydar@sbcglobal.net>
2021-02-05 05:08:55 +0000

    feat: Implements task dependency ordering

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -11,7 +11,7 @@ AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'start_date': datetime(2021, 2, 1),#datetime(2019, 1, 12),
 }
 
 dag = DAG('udac_example_dag',
@@ -63,3 +63,23 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
