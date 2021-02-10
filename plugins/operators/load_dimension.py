e74fd2636f9ea39c26866a0f2c93c40a91e23532
Seth Paydar <sethpaydar@sbcglobal.net>
2021-02-10 07:29:22 +0000

    feat: First implementation of LoadDimensionOperator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,34 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table="",
+                 redshift_conn_id="redshift",
+                 sql_statement="",
+                 truncate_insert_on=True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_statement = sql_statement
+        self.truncate_insert_on = truncate_insert_on
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('Starting LoadDimensionOperator for table ' + table)
+        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Initialized PostgresHook with Redshift credentials')
+        
+        if self.truncate_insert_on:
+            self.log.info('Truncate-insert mode is on. Deleting all rows from table ' + self.table + ' before insertion')
+            sql_statement = 'DELETE FROM %s' % self.table
+            redshift.run(sql_statement)
+            self.log.info('Redshift DELETE command complete')
+        else:
+            self.log.info('Truncate-insert mode is off. No records will be deleted before insertion')
+        
+        sql_statement = 'INSERT INTO %s %s' % (self.table, self.sql_statement)
+        self.log.info('Formatted SQL statement into INSERT command. Running INSERT command in Redshift')
+        
+        redshift.run(sql_statement)
+        self.log.info('Redshift INSERT command complete for table ' + self.table)
