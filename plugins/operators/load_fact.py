464083430913a071b8c3fdfb4f1ef251b9abf1ea
Seth Paydar <sethpaydar@sbcglobal.net>
2021-02-10 06:57:54 +0000

    feat: First implementation of LoadFactOperator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,15 +8,24 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table="",
+                 redshift_conn_id="redshift",
+                 sql_statement="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_statement = sql_statement
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info('Starting LoadFactOperator for table ' + self.table)
+        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info('Initialized PostgresHook with Redshift credentials')
+        
+        sql_statement = 'INSERT INTO %s %s' % (self.table, self.sql_statement)
+        self.log.info('Formatted SQL statement into INSERT command. Running INSERT command in Redshift')
+        
+        redshift.run(sql_statement)
+        self.log.info('Redshift INSERT command complete for table ' + self.table)
